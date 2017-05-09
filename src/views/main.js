d64c6f64512bf9ff194fabb56e5563434afd2bcf
Jakob Hohlfeld <jhohlfeld@polyban.io>
2017-05-09 09:53:51 +0200

    Add changes to build android version

--- src/views/main.js
+++ src/views/main.js
@@ -14,6 +14,7 @@ import {
 import routes from '../routes'
 import programs from '../../data/programs.json'
 import states from '../../data/states.json'
+import { colors } from '../styles'
 
 export default class Main extends Component {
   constructor (props) {
@@ -86,7 +87,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
-    marginTop: 20
+    marginTop: 20,
+    backgroundColor: colors.white
   },
   h1: {
     fontSize: 28,
